62ea4c58a21fb6de8ec0e375c8c776ea594cdead
ch-oi <gmu1233@gmail.com>
2020-08-20 16:38:03 +0800

    Merging with Blog's function

--- components/Chatroom/ChatroomItem.js
+++ components/Chatroom/ChatroomItem.js
@@ -3,8 +3,6 @@ import { View, Text, StyleSheet, Image } from 'react-native';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 import Colors from '../../constants/Colors';
 
-const imagelink = 'https://dog.ceo/img/dog.jpg';
-
 const ChatroomList = (props) => {
   return (
     <View>
